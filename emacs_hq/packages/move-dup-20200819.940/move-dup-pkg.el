3c1a1a1fa2618573f0a4b4bf80e1114d4fbaaa6f
Andreas Lindblad <ALindblad@gmail.com>
2021-01-14 12:22:18 +0100

    Add packages and pre-existing configuration

---
+++ emacs_hq/packages/move-dup-20200819.940/move-dup-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/andreas/emacs-conf/emacs_hq/packages/move-dup-20200819.940/move-dup.el  -*- no-byte-compile: t -*-
+(define-package "move-dup" "20200819.940" "Eclipse-like moving and duplicating lines or rectangles." 'nil :commit "c5a346d3058011b8152cceeb45858f9b4cef1b69" :authors '(("Jimmy Yuen Ho Wong" . "wyuenho@gmail.com")) :maintainer '("Jimmy Yuen Ho Wong" . "wyuenho@gmail.com") :keywords '("convenience" "text" "edit") :url "https://github.com/wyuenho/move-dup")
